e570af948a127118b0dec76146799e8693fdcefa
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2023-08-28 20:24:34 +0800

    ..

--- city_functions.py
+++
@@ -1,6 +0,0 @@
-
-def func(city, country, population=0):
-    if population != 0:
-        return f"{city}, {country} - Population: {population}".title()
-    else: 
-        return f"{city}, {country}"
